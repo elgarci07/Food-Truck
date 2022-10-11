39d47fc8b0a68a5f53fa3244339ee45208593376
Sergi <setapeses@gmail.com>
2022-10-11 18:00:43 +0200

    menu ok all pages

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,18 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "topnav") {
+//         x.className += " responsive";
+//     } else {
+//         x.className = "topnav";
+//     }
+// }
+
+var navList = document.getElementById("nav-lists");
+
+function Show() {
+    navList.classList.add("_Menus-show");
+}
+
+function Hide() {
+    navList.classList.remove("_Menus-show");
 }
